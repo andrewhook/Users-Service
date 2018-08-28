ed68260b85e6e9effbd832f39fca4c57d7c5682b
Andy Hook <me@andrewhook.uk>
2018-08-28 18:28:33 +0100

    adding tests and functionality for creating a new user

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
@@ -16,8 +17,30 @@ class UsersController extends Controller
         //
     }
 
+    /**
+     * Display a user listing.
+     * 
+     * @return [type] [description]
+     */
     public function index()
     {
         return User::all();
+    }
+
+    /**
+     * 
+     * 
+     * @param  Request $request [description]
+     * @return [type]           [description]
+     */
+    public function create(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'given_name' => 'required',
+            'family_name' => 'required',
+        ]);
+
+        return User::create($request->all());
     }
 }
--- tests/UsersTest.php
+++ tests/UsersTest.php
@@ -33,4 +33,32 @@ class UsersTest extends TestCase
         $this->assertEquals(200, $response->status());
         $this->assertEquals(User::all()->toJson(), $response->getContent());
     }
+
+    /**
+     * Test we can create a new user.
+     * 
+     * @return void
+     */
+    public function test_it_creates_a_new_user()
+    {
+        $user = ['email' => 'me@andrewhook.uk', 'given_name' => 'Andrew', 'family_name' => 'Hook'];
+
+        $this->json('POST', '/users', $user)
+             ->seeJson($user);
+    }
+
+    /**
+     * Ensure a new user isn't created if validation fails.
+     * 
+     * @return void
+     */
+    public function test_it_does_not_create_a_new_user_if_validation_fails()
+    {
+        $user = ['given_name' => 'Andrew', 'family_name' => 'Hook'];
+
+        $response = $this->call('POST', '/users', $user);
+
+        $this->assertEquals(422, $response->status());
+        $this->assertEquals(json_encode(['email' => ['The email field is required.']]), $response->getContent());
+    }
 }
