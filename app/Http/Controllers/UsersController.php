df4d3f880ebafe0a5413e426e3cda77ce4e7366c
Andy Hook <me@andrewhook.uk>
2018-08-28 20:31:47 +0100

    adding validation rule to ensure user emails are unique

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,19 +8,9 @@ use Illuminate\Http\Request;
 class UsersController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
      * Display a user listing.
      * 
-     * @return [type] [description]
+     * @return Illuminate\Database\Eloquent\Collection
      */
     public function index()
     {
@@ -28,15 +18,15 @@ class UsersController extends Controller
     }
 
     /**
+     * Create a user.
      * 
-     * 
-     * @param  Request $request [description]
-     * @return [type]           [description]
+     * @param  Request $request
+     * @return App\User
      */
     public function create(Request $request)
     {
         $this->validate($request, [
-            'email' => 'required|email',
+            'email' => 'required|email|unique:users,email',
             'given_name' => 'required',
             'family_name' => 'required',
         ]);
